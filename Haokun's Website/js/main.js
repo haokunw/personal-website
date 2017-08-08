53a33b722d5f6cd4d5be678619efe5cdaa1ba687
Haokun Wang <terryneverstop@gmail.com>
2017-08-07 23:22:24 -0700

    8/7/2017 update

---
+++ Haokun's Website/js/main.js
@@ -0,0 +1,14 @@
+// Make it rain!
+
+// -------------- nav ------------------ //
+
+// When the user click on the hamburger (.hamburger)
+  // toggle class mobile-navigation on .navigation
+
+
+// -------------- resume page ------------------ //
+
+// When the user click on ".resume-page h3"
+  // append <i class="fa fa-angle-double-up" aria-hidden="true"></i>
+  // remove <i class="fa fa-angle-double-down" aria-hidden="true"></i>
+  // slide toggle p
